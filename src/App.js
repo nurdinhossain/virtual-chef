802e025df3493eaf6f07ea380333a4a835c4adee
Nurdin <nurdinhossain@gmail.com>
2024-10-12 03:38:06 -0400

    Download pixel food images and add typing animation to main page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { TypeAnimation } from 'react-type-animation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TypeAnimation
+        sequence={[
+          '> Welcome to Virtual Chef.',
+          2000, 
+          '> Please choose a recipe.',
+          2000
+        ]}
+        wrapper="span"
+        speed={50}
+        style={{ fontSize: '2em', display: 'inline-block' }}
+        repeat={0}
+      />
       </header>
     </div>
   );
