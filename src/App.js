bd4b6247d83936ef81cd7cd56eb6120d41cc135f
Nurdin <nurdinhossain@gmail.com>
2024-10-12 18:18:57 -0400

    Add webcam

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { TypeAnimation } from 'react-type-animation';
 import { useState } from 'react';
+import Webcam from "react-webcam";
 import robot from "./robot.png"
 import burger from "./burger.png"
 import hotDog from "./hot-dog.png"
@@ -8,6 +9,7 @@ import pancakes from "./pancakes.png"
 import donut from "./donut.png"
 
 function App() {
+  const [robotVisible, setRobot] = useState(true);
   const [buttonsVisible, setButtons] = useState(false);
   const [dialogueStep, setDialogueStep] = useState(0);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
@@ -54,20 +56,20 @@ function App() {
       () => {
         document.getElementById("robot").classList.remove("App-robot")
       },
-      2000
+      () => setRobot(false)
     ]
   ];
 
   return (
     <div className="App">
       <header className="App-header">
-        <div className="robot-container">
+        {robotVisible && <div className="robot-container">
           <img id="robot" src={robot} width="50%" alt="Robot" />
           <img className="orbiting-image image1" src={burger} width="40%" alt="Burger" />
           <img className="orbiting-image image2" src={hotDog} width="40%" alt="Hot Dog" />
           <img className="orbiting-image image3" src={pancakes} width="40%" alt="Pancakes" />
           <img className="orbiting-image image4" src={donut} width="40%" alt="Donut" />
-        </div>
+        </div>}
         <TypeAnimation
           key={dialogueStep}
           sequence={dialogueSequences[dialogueStep]}
@@ -85,6 +87,21 @@ function App() {
             ))}
           </div>
         )}
+        {
+          !robotVisible && (
+            <div
+              style={{
+                padding: 20,
+                marginTop: 20,
+                backgroundColor: 'rgb(255, 0, 174)',
+                border: "3px solid black",
+                boxShadow: "10px 10px 5px rgba(0, 0, 0, 0.5)" // Add this line for a shadow effect
+              }}
+            >
+              <Webcam />
+            </div>
+          )
+        }
       </header>
     </div>
   );
